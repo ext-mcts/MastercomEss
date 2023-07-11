d42df8c59f3e643323b1db721a8dcfd5dbfcd7e8
annareddyharsha <harshavardhan4891@gmail.com>
2023-07-11 11:22:07 +0530

    Sign In and Out done

---
+++ application/controllers/Attendance.php
@@ -0,0 +1,113 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+require (APPPATH.'/libraries/REST_Controller.php');
+
+class Attendance extends REST_Controller
+{
+
+    public $userdetails;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("attendance_model");
+        $this->load->library('Authorization_Token');   
+
+        $this->userdetails = decode_token($this->input->get_request_header('Authorization')); // here we are calling helper
+
+        /* Start - this block is for avoiding CROS error */
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
+    
+        // Access-Control headers are received during OPTIONS requests
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");       
+    
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    
+            exit(0);
+        }
+        /* End - this block is for avoiding CROS error */
+
+    }
+
+    public function swipein_post()
+    {
+        $_GET['Page'] = $_GET['Page'] ?? 1;
+        $headers = $this->input->request_headers(); 
+        if (isset($headers['Authorization'])) 
+        {
+            $decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
+            if ($decodedToken['status'])
+            {
+                $_POST = json_decode(file_get_contents("php://input"), true);
+
+                $data = $this->attendance_model->CaptureSwipe($this->userdetails->EmployeeID,'In');// Inserting Employee
+
+                    if($data)
+                    {
+                        $message = array('message' => 'Swipe In!');
+                        $message['status'] = true;
+                        $this->response($message, REST_Controller::HTTP_CREATED);
+                    }
+                    else{ 
+                        $message = array('message' => 'Something went wrong!.');
+                        $message['status'] = false;
+                        $this->response($message, REST_Controller::HTTP_OK);
+                    }
+            }
+            else {
+                $this->response($decodedToken);
+            }
+        }
+        else {
+            $message = array('message' => 'Authentication failed');
+            $message['status'] = false;
+            $this->response($message, REST_Controller::HTTP_UNAUTHORIZED);
+        }
+    }
+
+    public function swipeout_post()
+    {
+        $_GET['Page'] = $_GET['Page'] ?? 1;
+        $headers = $this->input->request_headers(); 
+        if (isset($headers['Authorization'])) 
+        {
+            $decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
+            if ($decodedToken['status'])
+            {
+                $_POST = json_decode(file_get_contents("php://input"), true);
+
+                $data = $this->attendance_model->CaptureSwipe($this->userdetails->EmployeeID,'Out');// Inserting Employee
+
+                    if($data)
+                    {
+                        $message = array('message' => 'Swiped Out!.');
+                        $message['status'] = true;
+                        $this->response($message, REST_Controller::HTTP_CREATED);
+                    }
+                    else{ 
+                        $message = array('message' => 'Something went wrong!.');
+                        $message['status'] = false;
+                        $this->response($message, REST_Controller::HTTP_OK);
+                    }
+            }
+            else {
+                $this->response($decodedToken);
+            }
+        }
+        else {
+            $message = array('message' => 'Authentication failed');
+            $message['status'] = false;
+            $this->response($message, REST_Controller::HTTP_UNAUTHORIZED);
+        }
+    }
+}
---
+++ application/models/Attendance_model.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Attendance_model extends CI_Model {
+
+    public function __construct() {
+		
+		parent::__construct();
+	}
+
+    public function CaptureSwipe($empid,$type)
+    {
+        $sql = "INSERT INTO `mcts_extranet`.`dbo.attendancelogs` (EmployeeID, SwipeDate, SwipeTime, SwipeType) 
+                            VALUES ($empid, '".date('Y-m-d')."', '".date('H:i:s')."', '$type')";
+		$query=$this->db->query($sql);
+
+        if($query)
+            return true;
+        else
+            return false;
+    }
+}
