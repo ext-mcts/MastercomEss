48be4f518a7b717c41ae53f185ee8f264dae9d08
Suhassssss <104508217+Suhassssss@users.noreply.github.com>
2024-01-16 16:20:08 +0530

    cors

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/mcts_extranet-api';
+$config['base_url'] = 'http://192.168.4.60/MastercomEss';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/Employees.php
+++ application/controllers/Employees.php
@@ -21,7 +21,7 @@ class Employees extends REST_Controller
 
         /* Start - this block is for avoiding CROS error */
         if (isset($_SERVER['HTTP_ORIGIN'])) {
-            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header("Access-Control-Allow-Origin: *");
             header('Access-Control-Allow-Credentials: true');
             header('Access-Control-Max-Age: 86400');    // cache for 1 day
         }
